8b612942cf23e6156c03824fd16a9da9ac14c9be
Brahmbir <brahmbir68@gmail.com>
2025-06-08 20:01:43 +0530

    Set up GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',
+  basePath: '/githubMarkdownPreview',
+  assetPrefix: '/githubMarkdownPreview',
 };
 
 export default nextConfig;
